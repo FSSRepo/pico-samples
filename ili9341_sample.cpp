51b10c5b6bddcc81b6a09c19797dd8a163a6676f
FSSRepo <go778sgt@gmail.com>
2025-09-30 16:31:47 -0600

    ILI9341 Driver

--- ili9341_sample.cpp
+++ ili9341_sample.cpp
@@ -6,6 +6,8 @@ int main() {
     ILI9341* sc = new ILI9341(spi1, 11, 10, 8, 6, 9, 15);
     sc->start();
     sc->fillScreen(ILI9341_BLACK);
-    sc->drawPixel(8, 8, ILI9341_BLUE);
+    sc->fillRectangle(0, 0, 40, 40, ILI9341_RED);
+    sc->fillRectangle(40, 40, 10, 10, ILI9341_GREEN);
+    sc->fillRectangle(50, 50, 40, 40, ILI9341_BLUE);
     return 0;
 }
